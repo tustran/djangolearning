f655ab0e417a33c0f82692c6df4d24f005c5fac9
Tu Tran <tu.tran@eastagile.com>
2021-01-14 11:07:28 +0700

    Change database connection to Postgresql type

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject', 
+        'USER': 'postgres', 
+        'PASSWORD': '9499',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
