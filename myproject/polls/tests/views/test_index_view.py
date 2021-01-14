6435d6e2c85c3eca02664648ce25cba82cc85b52
Tu Tran <tu.tran@eastagile.com>
2021-01-14 16:34:48 +0700

    milestone: test add multi question index view

--- myproject/polls/tests/views/test_index_view.py
+++ myproject/polls/tests/views/test_index_view.py
@@ -1,17 +1,73 @@
+import datetime
 import unittest
 from django.test import Client
+from django.utils import timezone
+
+from polls.models import Question
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+def delete_all_questions():
+    Question.objects.all().delete()
+
+
 
 class IndexViewTest(unittest.TestCase):
-    def setUp(self):
-        # Every view test needs a client.
-        self.client = Client()
 
-    def test_index_view(self):
-        # Issues a GET request.
+    def setUp(self):
+        # Every view test needs a Client
+        self.client = Client()
+        delete_all_questions()
+
+    def test_index_view_with_no_question(self):
+        """
+        If no questions exist, the page should still response OK with no questions
+        """
+        response = self.client.get('/polls/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['questions']), 0)
+
+    def test_index_view_with_less_than_5_recent_questions(self):
+        # Create 3 questions
+        for i in range(3):
+            create_question(question_text="Question %d" % i, days=-i)
+
+        # Issues a GET request
         response = self.client.get('/polls/')
 
-        # Check that the response is 200 OK.
+        # Checks that the response is 200 OK
         self.assertEqual(response.status_code, 200)
 
-        # Check that the rendered context contains 5 questions.
+        # Checks that the response context contains 5 questions
+        self.assertEqual(len(response.context['questions']), 3)
+
+    def test_index_view_with_more_than_5_recent_questions(self):
+        # Create 10 questions
+        for i in range(10):
+            create_question(question_text="Question %d" % i, days=-i)
+
+        # Issues a GET request
+        response = self.client.get('/polls/')
+
+        # Checks that the response is 200 OK
+        self.assertEqual(response.status_code, 200)
+
+        # Checks that the response context contains 5 questions
         self.assertEqual(len(response.context['questions']), 5)
+
+    def test_index_view_with_some_future_questions(self):
+        # Create 6 questions, from past to future
+        # for range -2 to 4, should have 3 future questions
+        for i in range(-2, 4):
+            create_question(question_text="Question %d" % i, days=i)
+
+        # Issues a GET request
+        response = self.client.get('/polls/')
+
+        # Checks that the response is 200 OK
+        self.assertEqual(response.status_code, 200)
+
+        # Checks that the response context contains 3 questions
+        self.assertEqual(len(response.context['questions']), 3)
--- myproject/polls/views/index_view.py
+++ myproject/polls/views/index_view.py
@@ -18,16 +18,43 @@
 #                         }  # a dictionary (dict)
 #         return TemplateResponse(request, self.template, context)
 
+#######################################################################################
+
+# from django.template.response import TemplateResponse
+# from django.views.generic.base import TemplateView
+
+# from polls.models import Question
+
+# class IndexView(TemplateView):
+
+#     template_name = "polls/index.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['questions'] = Question.objects.all()
+#         return context
+
+#########################################################################################
+
 from django.template.response import TemplateResponse
-from django.views.generic.base import TemplateView
+from django.views.generic.base import View
+from django.utils import timezone
 
 from polls.models import Question
 
-class IndexView(TemplateView):
 
-    template_name = "polls/index.html"
+class IndexView(View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['questions'] = Question.objects.all()
-        return context
+    template = "polls/index.html"
+
+    def get(self, request):
+        context = {}
+        """
+        Returns the recent 5 questions published, not including those set
+        to be in the future
+        """
+        context['questions'] = Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by("-pub_date")[:5]
+
+        return TemplateResponse(request, self.template, context)
