06f65b49b8a301ede5f3c39fa3770907dc78d88c
Tu Tran <tu.tran@eastagile.com>
2021-01-14 16:17:29 +0700

    milestone: test view index page

--- myproject/polls/urls.py
+++ myproject/polls/urls.py
@@ -1,9 +1,9 @@
-from django.urls import include, re_path
+from django.urls import include, re_path, path
 from polls.views.index_view import IndexView
 from polls.views.question_view import QuestionView
 
 urlpatterns = (
-    re_path(r'^$', IndexView.as_view()),
+    path('', IndexView.as_view(), name="index"),
     re_path(r'^(?P<question_id>\d+)$', QuestionView.as_view(), name="poll_detail"),
     re_path(r'^(?P<question_id>\d+)/result$', QuestionView.as_view(template="polls/result.html"), name="poll_result"), # using same view but different template
 )
--- myproject/polls/views/index_view.py
+++ myproject/polls/views/index_view.py
@@ -25,19 +25,7 @@ from polls.models import Question
 
 class IndexView(TemplateView):
 
-    template = "polls/index.html"
-
-    # def get(self, request):
-    #     context = {}
-    #     context['var_string'] = "This Is A String" # a string
-    #     context['var_array'] = ["This", "Is", "An", "Array", ]  # an array of string
-    #     context['var_dict'] = {
-    #                         "part1": "This",
-    #                         "part2": "Is",
-    #                         "part3": "A",
-    #                         "part4": "Dict",
-    #                     }  # a dictionary (dict)
-    #     return TemplateResponse(request, self.template, context)
+    template_name = "polls/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
