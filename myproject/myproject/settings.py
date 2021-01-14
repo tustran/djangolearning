fb573b39d4bf0816e39fb5512eb7f2478f06f4e1
Tu Tran <tu.tran@eastagile.com>
2021-01-14 11:16:11 +0700

    Init polls app

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls', # remember to put comma at the end of the last item in a list
 ]
 
 MIDDLEWARE = [
