c521d89e5b07ef224970515d815c59191d73c91b
Tu Tran <tu.tran@eastagile.com>
2021-01-15 09:56:45 +0700

    today questions

--- myproject/polls/models/question_model.py
+++ myproject/polls/models/question_model.py
@@ -2,9 +2,21 @@ import datetime
 from django.utils import timezone
 from django.db import models
 
+class QuestionManager(models.Manager):
+
+    def published_recently(self):
+        exclude_list = []
+        for question in Question.objects.all():
+            if not question.was_published_recently():
+                exclude_list.append(question.id)
+
+        return Question.objects.all().exclude(id__in=exclude_list)
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200) # the poll question
     pub_date = models.DateTimeField('date published') # the published date
+
+    objects = QuestionManager()
 
     class Meta:
         app_label = "polls" # the app this model belong to
--- myproject/polls/views/index_view.py
+++ myproject/polls/views/index_view.py
@@ -53,8 +53,10 @@ class IndexView(View):
         Returns the recent 5 questions published, not including those set
         to be in the future
         """
-        context['questions'] = Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by("-pub_date")[:5]
+        # context['questions'] = Question.objects.filter(
+        #     pub_date__lte=timezone.now()
+        # ).order_by("-pub_date")[:5]
+
+        context['today_questions'] = Question.objects.published_recently().order_by("-pub_date")[:5]
 
         return TemplateResponse(request, self.template, context)
